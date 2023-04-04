72f4ff2a2a37aeb12ce258b8a0c60d92e60aa643
MatteoMancuso-92 <Matteotrieste@outlook.com>
2023-04-04 13:54:41 +0200

    javaScript

---
+++ script.js
@@ -0,0 +1,17 @@
+// DATI
+
+const name1 = prompt('Come ti chiami?');
+
+const lastName = prompt('il tuo cognome?');
+
+const favcolor = prompt('il tuo colore preferito');
+
+const currentYear = 23;
+
+// CALCOLO
+
+const password = name1 + lastName + favcolor + currentYear
+
+// ElEMENTO ID
+
+const eleSpan = document.getElementById('insicurissima').innerHTML = password
